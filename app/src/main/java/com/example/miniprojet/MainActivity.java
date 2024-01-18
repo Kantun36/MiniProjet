2faf3e915342741ea1a8087231b9b3ad00ae778d
Bogdan <bogdancichigin@gmail.com>
2024-01-18 11:06:48 +0100

    Update db

--- app/src/main/java/com/example/miniprojet/MainActivity.java
+++ app/src/main/java/com/example/miniprojet/MainActivity.java
@@ -4,10 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.firestore.FirebaseFirestore;
 
+public class MainActivity extends AppCompatActivity {
+    private final FirebaseFirestore db;
+
+    MainActivity(FirebaseFirestore db) {
+        this.db = db;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
