953f1fc5ed7da47aaab8d5e7cd58b1641eb4b5b6
Bogdan <bogdancichigin@gmail.com>
2024-01-25 10:56:54 +0100

    Teste pour afficher "nom" du restaurant

--- app/src/main/java/com/example/miniprojet/MainActivity.java
+++ app/src/main/java/com/example/miniprojet/MainActivity.java
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -25,6 +27,27 @@ public class MainActivity extends AppCompatActivity {
                         // Accédez aux données de chaque document
                         String restaurantName = document.getString("nom"); // Remplacez "nom" par le champ réel que vous souhaitez afficher
                         Log.d("MainActivity", "Nom du restaurant : " + restaurantName);
+                        // Créez un nouveau TextView pour afficher le nom du restaurant
+                        TextView restaurantTextView = new TextView(this);
+                        restaurantTextView.setText(restaurantName);
+
+                        Button restaurantButton = new Button(this);
+                        restaurantButton.setText(restaurantName);
+
+                        LinearLayout restaurantContainer = new LinearLayout(this);
+                        restaurantContainer.setOrientation(LinearLayout.HORIZONTAL);
+                        restaurantContainer.setPadding(50, 0, 50, 100);
+
+                        LinearLayout verticalLayoutInsideHorizontal = new LinearLayout(this);
+                        verticalLayoutInsideHorizontal.setOrientation(LinearLayout.VERTICAL);
+
+                        verticalLayoutInsideHorizontal.addView(restaurantTextView);
+                        verticalLayoutInsideHorizontal.addView(restaurantButton);
+
+                        restaurantContainer.addView(verticalLayoutInsideHorizontal);
+
+                        LinearLayout restaurantsContainer = findViewById(R.id.restaurantContainer);
+                        restaurantsContainer.addView(restaurantContainer);
                     }
                 })
                 .addOnFailureListener(e -> {
